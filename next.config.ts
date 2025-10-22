c85b532ea7e1091d532fa12c5723c483cbdb27c2
migueljgc <miguelgaviriam.8@gmail.com>
2025-10-22 00:49:24 -0500

    act secretariat

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
+const { i18n } = require("./next-i18next.config");
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["jxtyorqotieaseoelgfs.supabase.co"], // agrega tu dominio de Supabase
   },
+  reactStrictMode: true,
+  i18n,
 };
 
 export default nextConfig;
--- src/secretariat/secretariat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SecretariatService } from './secretariat.service';
-import { SecretariatController } from './secretariat.controller';
-
-@Module({
-  controllers: [SecretariatController],
-  providers: [SecretariatService],
-})
-export class SecretariatModule {}
